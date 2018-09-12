a01caea6a12927035e893ecf191261fa5c8b333c
KangningG <sakura_strife@sina.com>
2018-09-11 22:55:21 -0400

    update Lambda role privilege, update region

--- src/FinStopTimer.py
+++ src/FinStopTimer.py
@@ -5,22 +5,30 @@ import os
 # Enter the region your instances are in. Include only the region without specifying Availability Zone; e.g., 'us-east-1'
 
 def lambda_handler(event, context):
-    client = boto3.client('ec2')
-    region = os.environ.get('REGION')
+    # client = boto3.client('ec2')
+    if os.environ.get('REGION') == "":
+        fakeClient = boto3.client('ec2')
+        regions = [region['RegionName'] for region in fakeClient.describe_regions()['Regions']]
+    else:
+        regions = [os.environ.get('REGION')]
     taglist = os.environ.get('TAG')
     tags = list()
-    for tag in taglist.split(','):
-         tags.append(tag)
-    print(tags)
-    response = client.describe_instances(
-        Filters = [{'Name':'tag:Name', 'Values':tags},
-        {'Name':'instance-state-name', 'Values':['running']}]
-        )
-    InstanceList =  list()
-    for r in response['Reservations']:
-        # print(r['Instances'])
-        for instance in r['Instances']:
-            print(instance['InstanceId'])
-            InstanceList.append(instance['InstanceId'])
-    
-    client.stop_instances(InstanceIds = InstanceList)
+    for region in regions:
+        InstanceList =  list()
+        client = boto3.client('ec2', region_name=region)
+        for tag in taglist.split(','):
+            tags.append(tag)
+            response = client.describe_instances(
+                Filters = [{'Name':'tag:Name', 'Values':tags},
+                {'Name':'instance-state-name', 'Values':['running']}]
+                )
+        for r in response['Reservations']:
+            for instance in r['Instances']:
+                # print(instance['InstanceId'])
+                InstanceList.append(instance['InstanceId'])
+                # print("{0} : {1}".format(region,tag))
+        # print(InstanceList)
+        if not InstanceList:
+            continue
+        else:
+            client.stop_instances(InstanceIds = InstanceList)
